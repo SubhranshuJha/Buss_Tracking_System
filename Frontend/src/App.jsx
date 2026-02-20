37202ad1e2660bf763ce9375ea8324b7fcd90a5a
jeel <jeelmaheshwari2006@gmail.com>
2026-02-20 16:06:14 +0530

    changes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,18 @@
-import { BusProvider } from "./context/BusContext";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import Dashboard from "./pages/Dashboard";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <BusProvider>
-      <Dashboard />
-    </BusProvider>
+    <>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/track" element={<Home />} />
+      </Routes>
+    </>
   );
 }
 
