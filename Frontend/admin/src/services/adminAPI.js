9e6ed12edf1e7cc1bb837e944da9a9b4bd0b40bf
jeel <jeelmaheshwari2006@gmail.com>
2026-02-21 10:13:07 +0530

    route

--- Frontend/admin/src/services/adminAPI.js
+++ Frontend/admin/src/services/adminAPI.js
@@ -13,6 +13,8 @@ API.interceptors.request.use((req) => {
 
 export const addBus = (data) => API.post("/bus/", data);
 export const addRoute = (data) => API.post("/route", data);
+export const updateRoute = (id, data) => API.put(`/route/${id}`, data);
+export const deleteRoute = (id) => API.delete(`/route/${id}`);
 export const addTrip = (data) => API.post("/trip", data);
 export const adminLogin = (data) => API.post("/admin/login", data);
 export const getBuses = () => API.get("/bus");   // adjust if /bus
